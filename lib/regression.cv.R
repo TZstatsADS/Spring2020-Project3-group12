931f71efd3a1d7f5a0bfa599fba63832f5b8caf6
Ivan <iaw2110@columbia.edu>
2020-03-30 21:28:33 -0400

    Modifications to lasso and ridge code

--- lib/regression.cv.R
+++ lib/regression.cv.R
@@ -1,25 +1,13 @@
-regression.cv <- function(models, data, K=5) {
-  
-  train_scores <- c()
-  test_scores <- c()
-  
-  for(i in 1:K) {
+regression.cv <- function(models, data, K=5, alpha=1) {
     
-    sample <- sample.int(nrow(data), size = floor(0.8 * nrow(data)), replace = F)
-    train_data <- data[sample, ]
-    validation_data <- data[-sample, ]
+  X <- data[, -ncol(data)]
+  y <- data[, ncol(data)]
 
-    x_train <- as.matrix(train_data[, -ncol(train_data)])
-    x_validation <- as.matrix(validation_data[, -ncol(validation_data)])
-    
-    y_train <- as.factor(train_data[, ncol(train_data)])
-    y_validation <- as.factor(validation_data[, ncol(validation_data)])
-    
-    train_pred <- as.matrix(predict(models, s = 'lambda.min', newx= x_train, type = 'class'))
-    test_pred <- as.matrix(predict(models, s = 'lambda.min', newx= x_validation, type = 'class'))
-    
-    train_scores[i] <- mean(y_train == train_pred)
-    test_scores[i] <- mean(y_validation == test_pred)
-  }
-  return(c(mean(train_scores), mean(test_scores)))
+  ctrl <- trainControl(method="cv", number=K)
+  cvv <- train(X, y, method = "glmnet", 
+                    trControl = ctrl,
+                    tuneGrid = expand.grid(alpha = alpha, 
+                                           lambda = models$lambda.min))
+
+  return(cvv$results$Accuracy)
 }
--- lib/regression.test.R
+++ lib/regression.test.R
@@ -2,7 +2,7 @@
 ### Classification with testing data ###
 ########################################
 
-regression.test <- function(models, dat_test){
+regression.test <- function(models, dat_test, info){
   
   ### Input: 
   ###  - the fitted classification model using training data
@@ -13,5 +13,8 @@ regression.test <- function(models, dat_test){
 
   X_test <- as.matrix(dat_test)
   pred <- as.matrix(predict(models, s = 'lambda.min', newx = X_test, type = 'class'))
-  return(pred)
+  labels <- data.frame(Index = 1:nrow(dat_test), identity = rep(NA, nrow(dat_test)), pred)
+  colnames(labels)[3] <- "emotion_idx"
+  labels$identity <- info$identity
+  return(labels)
 }
